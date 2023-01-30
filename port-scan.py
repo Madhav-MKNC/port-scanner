9c51e1add65cd50920d4703c6f370a8ed6d9d799
MKNC <78339236+Madhav-MKNC@users.noreply.github.com>
2023-01-30 17:18:29 +0000

    Update port-scan.py

--- port-scan.py
+++ port-scan.py
@@ -1,52 +1,97 @@
+""" 
+This is a simple but very fast port scanner. It is able to scan for 10000 open ports in less than 10 seconds.
+We need to improve the accuracy.
+
+NEW FEATURE:  	get live status on a port.
+				specify it in sys.argv parameters as => 'python port-scan.py <hostname> -p<port>'
+				where <port1> is replaced by your specified port
+				full command eg: python port-scan.py localhost -p22
+
+by: Madhav MKNC
+"""
+
+# imports
 import socket
 import sys
+from time import time as now
+from time import sleep 
 
-from time import time
-init = time()
+# usage
+def usage():
+	print("-"*100)
+	print(f"USAGE [for port scaning]     --> python3 {sys.argv[0]} HOST_NAME START_PORT END_PORT")
+	print(f"                             --> python3 {sys.argv[0]} HOST_NAME")
+	print(f"      [for port live status] --> python3 {sys.argv[0]} HOST_NAME -p PORT")
+	print("-"*100)
+	sys.exit()
 
-if len(sys.argv) not in [4,5]:
-    print("-"*70)
-    print(" USAGE --> python port-scan.py HOST_NAME START_PORT END_PORT TIMEOUT")
-    print("-"*70)
-    sys.exit()
+# print screen
+def printScreen(): 
+	print("+"+"-"*21+"+")
+	print("|","SIMPLE PORT SCANNER","|")
+	print("+"+"-"*21+"+\n")
 
-print("+"+"-"*21+"+")
-print("|","SIMPLE PORT SCANNER","|")
-print("+"+"-"*21+"+\n+")
-timeout = float(input('=> Set timeout (=0.01 for fact scan with low accuracy; =1 for vice-versa): ')) if len(sys.argv) != 5 else float(sys.argv[4])
+# connecting to a port
+def port_open(host, port):
+	try:
+		with socket.socket() as s:
+			socket.setdefaulttimeout(1)
+			connection = s.connect_ex((host, int(port)))
+			return not bool(connection)
+	except Exception as err:
+		print(f"[!] error connecting to {host}:{port}")
+		print("[ERROR]",err)
+		return False
 
-try:
-    host = socket.gethostbyname(sys.argv[1])
-except socket.gaierror as e:
-    print(e)
-    sys.exit()
+# live status of a port 
+def live_rePORTS(host, port):
+	try:
+		print(f"[=] live status of port:{port} on host:{host}")
+		while True:
+			if port_open(host, port): print(f'[+] port {port} - OPEN    ',end='\r')
+			else: print(f'[-] port {port} CLOSED\t',end='\r')
+			sleep(3) # next iteration waits for 3 seconds
+	except KeyboardInterrupt:
+		sys.exit()
 
-openports = list()
-try:
-    i=1
-    for port in range(int(sys.argv[2]),int(sys.argv[3])+1):
-        s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        socket.setdefaulttimeout(timeout)
-        no_connection = s.connect_ex((host,port))
-        print(f'+ Scanning port {port}...',end="\r")
-        if not no_connection:
-            openports.append(port)
-            # banner = s.recv(1024) ## banner grabbing for some extra info
-            output = f'[{i}] OPEN port {port}'
-            print(output+' '*(50-len(output)))
-            i+=1
-    #    print(connection)
-        s.close()
-except KeyboardInterrupt:
-    pass
+# port scanner
+def scanports(host, start_port, end_port):
+	init = now()
+	openports = list()
 
-# for i in openports:
-#     print(f'+ port {i} OPEN')
+	try:
+		i=1
+		for port in range(int(start_port),int(end_port)+1):
+			print(f"[ ] Scanning port {port}",end='\r')
+			if port_open(host, port):
+				openports.append(port)
+				print(f"[{i}] port - {port} - OPEN")
+				i += 1 
+	except KeyboardInterrupt:
+		print("\n[-] keyboard interrupted")
 
-if not openports:
-    print('NO PORT OPEN IN THE GIVEN RANGE')
+	if not openports:
+		print('NO PORT OPEN IN THE GIVEN RANGE')
+	print("[+] scanned %d ports in %f seconds\n"%(port,now()-init))
 
-print("+"+' '*50)
-print("scanned %d ports in %f seconds\n"%(port,time()-init))
-    
-input('program finished')
+
+if __name__ == "__main__":
+	printScreen()
+
+	if '-h' in sys.argv or 'help' in sys.argv:
+		usage()
+	
+	try:
+		if '-p' in sys.argv:
+			if len(sys.argv)!=4: usage()
+			live_rePORTS(sys.argv[1], sys.argv[3])
+		elif len(sys.argv) == 2:
+			scanports(sys.argv[1], 1, 65535)
+		elif len(sys.argv) == 4:
+			HOST, START, END = map(str,sys.argv[1:])
+			scanports(HOST, START, END)
+		else:
+			usage()
+
+	except Exception as e: 
+		print("[ERROR]",e)
